b76122eb66fae6e9e73517554b97b7af40bfb21e
Poseyy <33185528+Poseyy@users.noreply.github.com>
2020-03-15 14:39:51 -0400

    GET 2

--- API/get.py
+++ API/get.py
@@ -2,9 +2,43 @@ import requests
 import json 
 
 
+# Replace with your token. You can get a token at https://spawner.ai
 token = ''
 
-url = "https://spawnerapi.com/fundamentals/" + token
+####### GET ENDPOINTS ########
+def fundamentals(): 
+    url = "https://spawnerapi.com/fundamentals/" + token
+    response = requests.get(url)
+    print(response.json())
 
-response = requests.get(url)
-print(response.json())
+def portfolio(): 
+    tickers = 'ge, aapl'
+    url = "https://spawnerapi.com/portfolio/" + tickers + '/' + token
+    response = requests.get(url)
+    print(response.json())
+
+def backtest(): 
+    start = '2012-02-23'
+    end = '2012-02-24'
+    tickers = 'ge, aapl'
+    url = "https://spawnerapi.com/backtest/" + start + "/" + end + "/" + tickers + "/" + token
+    response = requests.get(url)
+    print(response.json())
+
+def ta(): 
+    ticker = 'aapl'
+    url = "https://spawnerapi.com/ta/" + ticker + "/" + token
+    response = requests.get(url)
+    print(response.json())
+
+def basic_stats(): 
+    x = '1,2,3'
+    url = "https://spawnerapi.com/basic-stats/" + x + "/" + token
+    response = requests.get(url)
+    print(response.json())
+
+#fundamentals()
+#portfolio()
+#backtest() 
+#ta()
+#basic_stats()
