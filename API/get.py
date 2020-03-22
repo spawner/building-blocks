0c85f72fbd2926f8769f61ce972eba87f5cb226c
Poseyy <33185528+Poseyy@users.noreply.github.com>
2020-03-21 23:55:22 -0400

    add metrics to get.py

--- API/get.py
+++ API/get.py
@@ -36,9 +36,58 @@ def basic_stats():
     url = "https://spawnerapi.com/basic-stats/" + x + "/" + token
     response = requests.get(url)
     print(response.json())
+    
+def volatility(): 
+    x = 'AMD'
+    url = "https://spawnerapi.com/volatility/" + x + "/" + token
+    response = requests.get(url)
+    print(response.text)
 
-#fundamentals()
-#portfolio()
-#backtest() 
-#ta()
-#basic_stats()
+def expected_returns(): 
+    x = 'AMD'
+    url = "https://spawnerapi.com/expected-return/" + x + "/" + token
+    response = requests.get(url)
+    print(response.text)
+
+def max_drawdown(): 
+    x = 'AMD'
+    url = "https://spawnerapi.com/max-drawdown/" + x + "/" + token
+    response = requests.get(url)
+    print(response.text)
+
+def sharpe(): 
+    x = 'AMD'
+    url = "https://spawnerapi.com/sharpe/" + x + "/" + token
+    response = requests.get(url)
+    print(response.text)
+
+def calmar(): 
+    x = 'AMD'
+    url = "https://spawnerapi.com/calmar/" + x + "/" + token
+    response = requests.get(url)
+    print(response.text)
+
+def sortino(): 
+    x = 'AMD'
+    url = "https://spawnerapi.com/sortino/" + x + "/" + token
+    response = requests.get(url)
+    print(response.text)
+
+def value_at_risk(): 
+    x = 'AMD'
+    url = "https://spawnerapi.com/value-at-risk/" + x + "/" + token
+    response = requests.get(url)
+    print(response.text)
+
+def kelly_criterion(): 
+    x = 'AMD'
+    url = "https://spawnerapi.com/kelly-criterion/" + x + "/" + token
+    response = requests.get(url)
+    print(response.text)
+
+def implied_volatility(): 
+    x = 'AMD'
+    url = "https://spawnerapi.com/implied-volatility/" + x + "/" + token
+    response = requests.get(url)
+    print(response.text)
+
